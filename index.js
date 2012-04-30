7b44c985c2d9646f8c873f0405911427a241ea66
Tom Wilson <tom@jackhq.com>
2012-04-30 10:55:40 -0400

    update to get all assets after 30sec

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var pin = require('linchpin'),
   log = require('./lib/log');
 
 // find file if route not handled
-pin.on('*', function(req, res) {
-  if(!req.handled && req.method === 'GET') { filed('./public' + req.url).pipe(res); }
+pin.on('**', function(req, res) {
+  setTimeout ( -> if(!req.handled && req.method === 'GET') { filed('./public' + req.url).pipe(res); }), 30000
 });
 
 function Apprentice() {
