f5e7b2c43eb314b6c6a6bb9b34c3585b6ef512a6
Tom Wilson <tom@jackhq.com>
2012-06-03 07:58:45 -0400

    update package

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var pin = require('linchpin'),
   mime = require('./lib/mime'),
   log = require('./lib/log');
 
-pin.on('GET/assets/**', function(req, res) {
-  var resource = req.url.replace(/assets/,'public');
-  filed('.' + resource).pipe(res);
-});
+// pin.on('GET/assets/**', function(req, res) {
+//   var resource = req.url.replace(/assets/,'public');
+//   filed('.' + resource).pipe(res);
+// });
 
 function Apprentice() {
   var self = this;
@@ -18,17 +18,15 @@ function Apprentice() {
   self.httpServer = http.createServer();
 
   self.httpServer.on('request', function(req, res) {
-    var path = url.parse(req.url).pathname, route = req.method;
+    var path = url.parse(req.url).pathname;
     
     res.text = function(body, statusCode) {
-      req.handled = true;
       if (statusCode == null || statusCode == 'undefined') { statusCode = 200 };
       res.writeHead(statusCode, { 'content-type': mime.text });
       res.end(body);
     }
     
     res.json = function(body, statusCode) {
-      req.handled = true;
       if (statusCode == null || statusCode == 'undefined') { statusCode = 200 };
       try { 
         body = JSON.stringify(body);
@@ -42,20 +40,23 @@ function Apprentice() {
     }
     
     res.html = function(body, statusCode) {
-      req.handled = true;
       if (statusCode == null || statusCode == 'undefined') { statusCode = 200 };
       res.writeHead(statusCode, {'content-type': mime.html });
       res.end(body);
     }
     
     setResource(path, req);
-    if (path.length > 1) route += url.parse(req.url).pathname;
+    //if (path.length > 1) route += url.parse(req.url).pathname;
     req.body = "";
-    req.on('data', function(data) { req.body += data; });
-    req.on('end', function() { 
-      if (req.body.length > 0) { parseBody(req); }
-      pin.emit(route, req, res); 
-    });
+    if (req.headers['content-type'].match(/(application\/json|application\/x-www-form-urlencoded)/) !== null) {
+      req.on('data', function(data) { req.body += data; });
+      req.on('end', function() { 
+        if (req.body.length > 0) { parseBody(req); }
+        pin.emit('REQ' + req.url, req, res); 
+      });
+    } else {
+      pin.emit('REQ' + req.url, req, res); 
+    }
   });
   
   self.start = function(port, cb) {
